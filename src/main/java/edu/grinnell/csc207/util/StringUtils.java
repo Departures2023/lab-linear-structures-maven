251f7c1b4ec5a20af01c5d38f80e5b40b9493b25
KevinTang <tangkevi@grinnell.edu>
2024-10-03 14:58:03 -0500

    Implement the  method.

--- src/main/java/edu/grinnell/csc207/util/StringUtils.java
+++ src/main/java/edu/grinnell/csc207/util/StringUtils.java
@@ -1,9 +1,7 @@
 package edu.grinnell.csc207.util;
 
-import edu.grinnell.csc207.linear.Stack;
 import edu.grinnell.csc207.linear.LinkedStack;
-
-import java.io.PrintWriter;
+import edu.grinnell.csc207.linear.Stack;
 
 /**
  * Assorted utilities for working with strings.
@@ -19,9 +17,44 @@ public class StringUtils {
   /**
    * Determine whether the parens match in string.
    */
-  public static boolean checkMatching(String str) {
+  public static boolean checkMatching(String str) throws Exception{
     Stack<Character> parens = new LinkedStack<Character>();
-    return false;       // STUB
+
+    /*
+     * Step through the characters in the expression
+    When you encounter an open paren or open square bracket, push it 
+      on the stack
+    When you encounter a close paren or close square bracket, pop 
+      the corresponding opening character off the stack.  If the two
+      characters don't match, issue an error.
+    If you encounter a closing character with an empty stack, that 
+      close paren or bracket is mismatched.
+If the stack is not empty, there are unmatched open or closed parens.
+     */
+
+    for (int i = 0; i < str.length(); i++) {
+      Character strChar = str.charAt(i);
+      if (strChar == '(' || strChar == '[') {
+        parens.push(strChar);
+      } else if (strChar == ')' || strChar == ']') {
+        if (parens.isEmpty()) {
+          return false;
+        }
+        Character popOff = parens.pop();
+        switch (strChar) {
+          case ')':
+            if (popOff != '(') {
+              return false;}
+            break;
+          case ']':
+            if (popOff != '['){ 
+              return false;}
+            break;
+        }
+      }
+    }
+
+    return parens.isEmpty();
   } // checkMatching
 } // class StringUtils    
 
